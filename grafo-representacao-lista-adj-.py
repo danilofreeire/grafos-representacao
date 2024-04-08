98db865ed086a0dcf9c4979c4b0b371ac8682dc2
Danilo Freire <danilofreirea90@gmail.com>
2024-04-08 18:39:29 -0300

    Criação Classe Edge

--- grafo-representacao-lista-adj-.py
+++ grafo-representacao-lista-adj-.py
@@ -11,3 +11,13 @@ class Node:
         return s
     def add_neighbor(self,node):
         self.adj.append(node) # ADICIONA O NÓ 'NODE' A LISTA 'SELF.ADJ'
+        
+class Edge:
+    def __init__(self,from_=None,to=None,weight=None,active=True):
+        self.from_ = from_ # NÓ DE ORIGEM
+        self.to_ = to_ # NÓ DE DESTINO
+        self.weight = weight  #PESO
+        self.active = active #SE O NÓ ESTÁ ATIVO
+    def to_str(self):
+        return f"{'{'}{self.from_.tag},{self.to_.tag},{self.weight}{'}'}" #{ NOME NÓ ORIGEM , NOME NÓ DESTINO , PESO }
+    
